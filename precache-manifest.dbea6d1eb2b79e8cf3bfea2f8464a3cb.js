ad6d0d106d810caa397024dd6c5738aafcfaf7b7
eldhosevj <eldhosvj@gmail.com>
2022-03-29 18:27:26 -0700

    Updates

---
+++ precache-manifest.dbea6d1eb2b79e8cf3bfea2f8464a3cb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c9b7d6fcbfacd13123d",
+    "url": "/static/js/main.e0f2d1d3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f550aac87ae1bed0f2dc",
+    "url": "/static/js/2.53908e57.chunk.js"
+  },
+  {
+    "revision": "831021d7c3fd094a6d0cf838544afd18",
+    "url": "/index.html"
+  }
+];
